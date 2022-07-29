19b933bfcec7e51456a31c6346dbf50826c7f70f
atulkumarmanjhi <atulkashyapvlog@gmail.com>
2022-07-29 10:57:42 +0530

    first comite from atul c++

---
+++ conditionandstatment.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{ 
+  int a = 9;
+  if(a == 9)
+{
+    cout<< "nine" << endl;
+
+}
+
+if(a > 0)
+{
+    cout<< "Positive";
+}
+else
+{
+    cout<< "nagative";
+}
+    
+}
---
+++ q1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    int b = a+1;
+
+    cout<<b;
+    /*
+    if((a=3)==b)
+    {
+        cout<<a<<endl;
+    }
+    else
+    {
+        cout<<a+1;
+    }
+    */
+
+}
